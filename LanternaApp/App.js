baac4724f39e4d48a2286959609fd18a445a3adf
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-06-29 21:18:27 -0300

    Adicionando Modifcações de estilo para on / off

--- LanternaApp/App.js
+++ LanternaApp/App.js
@@ -1,112 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import imageOn from './assents/icons/eco-light.png';
+import imageOff from './assents/icons/eco-light-off.png';
+import imageDioOn from './assents/icons/logo-dio.png';
+import imageDioOff from './assents/icons/logo-dio-white.png';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const toggle = false; //false
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity onPress={()=> {}}>
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={toggle ? imageOn : imageOff}>
+        </Image>
+
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={toggle ? imageDioOn : imageDioOff}>
+        </Image>
+      </TouchableOpacity>
     </View>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: "center",
+    justifyContent: "center",
+    width: 150,
+    height: 150,
+  },
+
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: "center",
+    justifyContent: "center",
+    tintColor: 'white',
+    width: 250,
+    height: 150,
+  },
+
+  logoDio: {
+    resizeMode: 'contain',
+    alignSelf: "center",
+    justifyContent: "center",
+    width: 250,
+    height: 150,
+  },
+  
+});
